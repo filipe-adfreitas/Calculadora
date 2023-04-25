627416c93a8f071ef0d62cee49189fe45b621d03
Filipe Alves <filipe.adfreitas@gmail.com>
2023-04-24 23:17:43 -0300

    first commit

---
+++ script/index.js
@@ -0,0 +1,23 @@
+function insert(num) {
+    let numero = document.getElementById('visor').innerHTML;
+    document.getElementById('visor').innerHTML = numero + num
+};
+
+function limparTudo() {
+    document.getElementById('visor').innerHTML = "";
+};
+
+function limparUm() {
+    let numerosNoVisor = document.getElementById('visor').innerHTML;
+    document.getElementById('visor').innerHTML = numerosNoVisor.substring(0, numerosNoVisor.length -1)
+};
+
+function calcular() {
+    let resultado = document.getElementById('visor').innerHTML;
+    if (visor) {
+        document.getElementById('visor').innerHTML = eval(resultado);
+    }
+     else {
+        document.getElementById('visor').innerHTML = "0";
+    }
+}
